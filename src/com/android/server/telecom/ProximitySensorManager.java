4823f6bb54b3916fe884aef3b6b38173586071e3
martinusbe <martinusbe@gmail.com>
2015-08-28 09:05:59 +0200

    Revert "telecom: Move CMHW to CMSDK"
    This reverts commit 6eea5e67f1a867b41d904e83deca2e8fc4f3b02c.

--- src/com/android/server/telecom/ProximitySensorManager.java
+++ src/com/android/server/telecom/ProximitySensorManager.java
@@ -17,9 +17,8 @@
 package com.android.server.telecom;
 
 import android.content.Context;
+import android.hardware.CmHardwareManager;
 import android.os.PowerManager;
-
-import cyanogenmod.hardware.CMHardwareManager;
 
 /**
  * This class manages the proximity sensor and allows callers to turn it on and off.
@@ -29,11 +28,11 @@ public class ProximitySensorManager extends CallsManagerListenerBase {
 
     private final PowerManager.WakeLock mProximityWakeLock;
     private boolean mWasTapToWakeEnabled = false;
-    private final CMHardwareManager mHardware;
+    private final CmHardwareManager mCmHardwareManager;
 
     public ProximitySensorManager(Context context) {
         PowerManager pm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
-        mHardware = CMHardwareManager.getInstance(context);
+        mCmHardwareManager = (CmHardwareManager) context.getSystemService(Context.CMHW_SERVICE);
         if (pm.isWakeLockLevelSupported(PowerManager.PROXIMITY_SCREEN_OFF_WAKE_LOCK)) {
             mProximityWakeLock = pm.newWakeLock(
                     PowerManager.PROXIMITY_SCREEN_OFF_WAKE_LOCK, TAG);
@@ -67,10 +66,10 @@ public class ProximitySensorManager extends CallsManagerListenerBase {
         if (!mProximityWakeLock.isHeld()) {
             Log.i(this, "Acquiring proximity wake lock");
             mProximityWakeLock.acquire();
-            if (mHardware.isSupported(CMHardwareManager.FEATURE_TAP_TO_WAKE)) {
+            if (mCmHardwareManager.isSupported(CmHardwareManager.FEATURE_TAP_TO_WAKE)) {
                 mWasTapToWakeEnabled =
-                        mHardware.get(CMHardwareManager.FEATURE_TAP_TO_WAKE);
-                mHardware.set(CMHardwareManager.FEATURE_TAP_TO_WAKE, false);
+                        mCmHardwareManager.get(CmHardwareManager.FEATURE_TAP_TO_WAKE);
+                mCmHardwareManager.set(CmHardwareManager.FEATURE_TAP_TO_WAKE, false);
             }
         } else {
             Log.i(this, "Proximity wake lock already acquired");
@@ -86,9 +85,9 @@ public class ProximitySensorManager extends CallsManagerListenerBase {
             return;
         }
         if (mProximityWakeLock.isHeld()) {
-            if (mHardware.isSupported(CMHardwareManager.FEATURE_TAP_TO_WAKE)
+            if (mCmHardwareManager.isSupported(CmHardwareManager.FEATURE_TAP_TO_WAKE)
                     && mWasTapToWakeEnabled) {
-                mHardware.set(CMHardwareManager.FEATURE_TAP_TO_WAKE, true);
+                mCmHardwareManager.set(CmHardwareManager.FEATURE_TAP_TO_WAKE, true);
             }
             Log.i(this, "Releasing proximity wake lock");
             int flags =
